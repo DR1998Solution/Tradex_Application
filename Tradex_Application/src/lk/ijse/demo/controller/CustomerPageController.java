f68ebb79e71dcb06d241100ba03b2d736f046027
DilshanRajika9835 <dilshanrajitha1998@gmail.com>
2021-10-09 09:45:34 +0530

    create customer UI using Java FX

--- Tradex_Application/src/lk/ijse/demo/controller/CustomerPageController.java
+++ Tradex_Application/src/lk/ijse/demo/controller/CustomerPageController.java
@@ -8,6 +8,10 @@
  */
 
 package lk.ijse.demo.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 /**
  * @author Dilshan Rajika Withanachchi <dilshanrajitha1998@gmail.com>
  * @Time :8:56 AM
@@ -15,4 +19,10 @@ package lk.ijse.demo.controller;
  * @Project Name :Tradex_Application
  */
 public class CustomerPageController {
+    public TextField txtid;
+    public TextField txtname;
+
+    public void btnsaveOnAction(ActionEvent actionEvent) {
+
+    }
 }
